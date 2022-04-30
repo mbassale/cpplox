e16747dae0eeb919f76da6f2a137f231636d6185
Marco Bassaletti <bassaletti@gmail.com>
2022-04-29 23:41:53 -0700

    [CppLox][Parser] Initial recursive descent parser

---
+++ src/parser.cpp
@@ -0,0 +1,145 @@
+#include "parser.h"
+
+namespace cpplox {
+
+ast::ProgramPtr Parser::parse() {
+  bool keepRunning = true;
+
+  ast::ProgramPtr programNode = std::make_shared<ast::Program>();
+
+  previous = Token();
+  while (keepRunning) {
+    current = scanner.next();
+    if (current.type == TOKEN_EOF) {
+      break;
+    }
+    const auto stmt = statement();
+    programNode->statements.push_back(stmt);
+  }
+
+  return programNode;
+}
+
+/**
+ * statement:
+ *  exprStmt
+ *  | forStmt
+ *  | ifStmt
+ *  | printStmt
+ *  | returnStmt
+ *  | whileStmt
+ *  | block ;
+ *
+ * @return ast::Statement
+ */
+ast::StatementPtr Parser::statement() {
+  if (match(TOKEN_LEFT_BRACE)) {
+    return std::static_pointer_cast<ast::Statement>(block());
+  } else if (match(TOKEN_FOR)) {
+    return std::static_pointer_cast<ast::ForStatement>(forStatement());
+  } else {
+    return std::static_pointer_cast<ast::Statement>(expressionStatement());
+  }
+}
+
+ast::BlockPtr Parser::block() {
+  auto blockStmt = std::make_shared<ast::Block>();
+  while (!match(TOKEN_RIGHT_BRACE) && isAtEnd()) {
+    auto stmt = statement();
+    blockStmt->statements.push_back(stmt);
+  }
+  return blockStmt;
+}
+
+ast::ForStatementPtr Parser::forStatement() {
+  ast::ExpressionPtr initializer{nullptr};
+  if (!match(TOKEN_SEMICOLON)) {
+    initializer = expression();
+  }
+  ast::ExpressionPtr condition{nullptr};
+  if (!match(TOKEN_SEMICOLON)) {
+    condition = expression();
+  }
+  ast::ExpressionPtr increment{nullptr};
+  if (!match(TOKEN_SEMICOLON)) {
+    increment = expression();
+  }
+  auto forStmt = std::make_shared<ast::ForStatement>();
+  forStmt->initializer = initializer;
+  forStmt->condition = condition;
+  forStmt->increment = increment;
+  return std::static_pointer_cast<ast::ForStatement>(forStmt);
+}
+
+/**
+ * expressionStatement: expression ";" ;
+ *
+ * @return ast::ExpressionStatement
+ */
+ast::ExpressionStatementPtr Parser::expressionStatement() {
+  const auto expr = expression();
+  consume(TOKEN_SEMICOLON, "Missing semicolon.");
+  auto exprStmt = std::make_shared<ast::ExpressionStatement>();
+  exprStmt->expression = expr;
+  return exprStmt;
+}
+
+ast::ExpressionPtr Parser::expression() {
+  return std::static_pointer_cast<ast::Expression>(primary());
+}
+
+/**
+ * primary:
+ *  "true"
+ *  | "false"
+ *  | "nil"
+ *  | "this"
+ *  | NUMBER
+ *  | STRING
+ *  | IDENTIFIER
+ *  | "(" expression ")"
+ *  | "super" "." IDENTIFIER ;
+ *
+ * @return ast::ExpressionPtr
+ */
+ast::ExpressionPtr Parser::primary() {
+  switch (current.type) {
+    case TOKEN_TRUE:
+    case TOKEN_FALSE:
+    case TOKEN_NIL:
+    case TOKEN_NUMBER:
+    case TOKEN_STRING:
+    case TOKEN_IDENTIFIER:
+      return std::static_pointer_cast<ast::Expression>(
+          ast::Literal::make(current));
+    case TOKEN_LEFT_PAREN: {
+      const auto expr = expression();
+      consume(TOKEN_RIGHT_PAREN, "Unbalanced parenthesis.");
+      return expr;
+    }
+    default:
+      throw std::runtime_error("Unknown primary.");
+  }
+}
+
+bool Parser::match(TokenType tokenType) {
+  if (current.type == tokenType) {
+    advance();
+    return true;
+  }
+  return false;
+}
+
+Token Parser::advance() {
+  current = scanner.next();
+  return current;
+}
+
+void Parser::consume(TokenType tokenType, const std::string& error_message) {
+  if (match(tokenType)) {
+    advance();
+  }
+  throw std::runtime_error(error_message);
+}
+
+}  // namespace cpplox
---
+++ src/parser.h
@@ -0,0 +1,38 @@
+#ifndef __cpplox_h_parser_h
+#define __cpplox_h_parser_h
+
+#include "ast.h"
+#include "common.h"
+#include "scanner.h"
+
+namespace cpplox {
+
+class Parser {
+ private:
+  Token previous;
+  Token current;
+  Scanner& scanner;
+
+ public:
+  Parser(Scanner& scanner) : scanner(scanner), previous(), current() {}
+
+  ast::ProgramPtr parse();
+
+ private:
+  ast::BlockPtr block();
+  ast::StatementPtr statement();
+  ast::ForStatementPtr forStatement();
+  ast::ExpressionStatementPtr expressionStatement();
+  ast::ExpressionPtr expression();
+  ast::ExpressionPtr primary();
+
+  bool isAtEnd() { return current.type == TOKEN_EOF; }
+  bool match(TokenType tokenType);
+  Token peek() { return current; }
+  Token advance();
+  void consume(TokenType tokenType, const std::string& error_message);
+};
+
+}  // namespace cpplox
+
+#endif  // __cpplox_h_parser_h
