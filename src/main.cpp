255bc218ce82d00e295bfd80810711bcfba9ee33
Marco Bassaletti <bassaletti@gmail.com>
2022-06-19 05:17:43 +0000

    [CppLox] Add GLog to main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -52,21 +52,25 @@ class Driver {
 
   InterpretResult interpret(const std::string &source) {
     try {
+      VLOG(0) << "======== PARSING START ========";
       Scanner scanner(source);
       cpplox::Parser parser(scanner);
       const auto program = parser.parse();
       if (parser.hasErrors()) {
         for (const cpplox::ParserException &error : parser.getErrors()) {
-          std::cerr << error.what() << std::endl;
+          LOG(ERROR) << error.what();
         }
         return InterpretResult::INTERPRET_PARSING_ERROR;
       }
+      VLOG(0) << "======== PARSING END ========";
+
+      VLOG(0) << "======== COMPILING START ========";
       const auto compilerConfig = buildCompilerConfig();
       cpplox::CompilerV2 compiler(compilerConfig);
       const auto script = compiler.compile("main", program);
       if (compiler.hasErrors()) {
         for (const std::string &error : compiler.getErrors()) {
-          std::cerr << error << std::endl;
+          LOG(ERROR) << error;
         }
         return InterpretResult::INTERPRET_COMPILE_ERROR;
       }
@@ -76,16 +80,17 @@ class Driver {
         OpCodePrinter printer(script->getChunk(), instructions);
         printer.print();
       }
+      VLOG(0) << "======== COMPILING END ========";
 
-      if (FLAGS_trace) {
-        std::cout << "== execution ==" << std::endl;
-      }
-      return vm.interpret(script);
+      VLOG(0) << "======== EXECUTION START ========";
+      const auto interpretResult = vm.interpret(script);
+      VLOG(0) << "======== EXECUTION END ========";
+      return interpretResult;
     } catch (VMRuntimeError &err) {
-      std::cerr << "RuntimeError: " << err.what() << std::endl;
+      LOG(ERROR) << "RuntimeError: " << err.what();
       return InterpretResult::INTERPRET_RUNTIME_ERROR;
     } catch (cpplox::CompilerError &err) {
-      std::cerr << "CompilerError: " << err.what() << std::endl;
+      LOG(ERROR) << "CompilerError: " << err.what();
       return InterpretResult::INTERPRET_COMPILE_ERROR;
     }
   }
@@ -100,13 +105,13 @@ class Driver {
 };
 
 int main(int argc, char *argv[]) {
-  Driver driver;
-
   std::ostringstream usage_ss;
   usage_ss << argv[0] << " [script-path]";
   gflags::SetUsageMessage(usage_ss.str());
-  gflags::ParseCommandLineFlags(&argc, &argv, false);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
 
+  Driver driver;
   if (argc == 1) {
     driver.repl();
   } else if (argc == 2) {
