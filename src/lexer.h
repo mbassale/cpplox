7d405af20652200320cbc09fc854d8d2ff149343
Marco Bassaletti <bassaletti@gmail.com>
2023-04-22 00:51:38 -0700

    [Lexer] Add missing header for PythonLexer

---
+++ src/lexer.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <FlexLexer.h>
+
+#include <iostream>
+#include <stack>
+#include <string>
+#include "ast.h"
+#include "astbuilder.h"
+#include "python_parser.hpp"  // Include Bison-generated header file for token definitions
+
+class PythonLexer : public yyFlexLexer {
+ public:
+  PythonLexer(std::istream *in) : yyFlexLexer(in) {}
+};
+
+int yylex(PythonParser::PythonParser::value_type* value, ASTBuilder& builder,
+          PythonLexer& lexer);
